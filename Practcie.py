f637a0a38c34b7dcca42a39e6dafccbeb8dba99f
woochanleee <030219woo@naver.com>
2019-05-31 17:02:18 +0900

    while

--- Practcie.py
+++ Practcie.py
@@ -1,8 +1,6 @@
-pocket = ['paper', 'cellphone', 'money']
-a = 1
-if 'card' in pocket:
-    pass
-elif a:
-    print("hello")
-else:
-    print("no money")
+treeHit = 0
+while treeHit < 10:
+    treeHit = treeHit + 1
+    print("나무를 %d번 찍었습니다." %treeHit)
+    if treeHit == 10:
+        print("나무 넘어가요~~~~조심")
